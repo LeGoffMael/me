fef514d2ab5561c2faf71e637e965c5a55b19496
Maël Le Goff <legoffmael@gmail.com>
2020-10-03 15:49:57 +0200

    change links depending the language

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ i18next.init({
             name: 'Maël Le Goff',
             position: 'Software Engineer',
             description: [
-                'I am a Software Engineer recently <a href="https://www.utbm.fr/" target="_blank">graduated</a>.',
+                'I am a Software Engineer recently <a href="https://www.utbm.fr/english/" target="_blank">graduated</a>.',
                 'Born in France, I am now based <s>in Seoul, South Korea working as a full stack developer and UI designer <a href="https://www.xenoimpact.com/" target="_blank">@XenoImpact</a></s>.',
-                '<a href="http://www.v2.legoffmael.fr/resources/RESUME_LE_GOFF_MAEL.pdf" target="_blank">Check out</a> my resume or <a href="mailto:legoffmael@gmail.com" target="_blank">reach out</a> if you are interested by my works.'
+                '<a href="https://www.legoffmael.fr/resources/RESUME_LE_GOFF_MAEL.pdf" target="_blank">Check out</a> my resume or <a href="mailto:legoffmael@gmail.com" target="_blank">reach out</a> if you are interested by my works.'
             ],
             location: 'in <s>South Korea</s>',
             credits: [
@@ -35,7 +35,7 @@ i18next.init({
             description: [
                 'Je suis un Ingénieur informatique récemment <a href="https://www.utbm.fr/" target="_blank">diplômé</a>.',
                 'Né en France, je vis désormais <s>à Séoul, Corée du Sud travaillant en tant que développeur full stack et UI designer <a href="https://www.xenoimpact.com/" target="_blank">@XenoImpact</a></s>.',
-                '<a href="http://www.v2.legoffmael.fr/resources/RESUME_LE_GOFF_MAEL.pdf" target="_blank">Téléchargez</a> mon CV ou <a href="mailto:legoffmael@gmail.com" target="_blank">contactez-moi</a> si vous êtes intéressé par mon travail.'
+                '<a href="https://www.legoffmael.fr/resources/CV_LE_GOFF_MAEL.pdf" target="_blank">Téléchargez</a> mon CV ou <a href="mailto:legoffmael@gmail.com" target="_blank">contactez-moi</a> si vous êtes intéressé par mon travail.'
             ],
             location: 'en <s>Corée du Sud</s>',
             credits: [
